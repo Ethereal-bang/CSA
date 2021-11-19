78cb77c6d13fbbdd5ac0398bdff9824a4ea990da
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-11-20 00:53:02 +0800

    update： 递归实现深拷贝

--- JS深入+沈若凤+2020211785/Level 3-对象深拷贝.js
+++ JS深入+沈若凤+2020211785/Level 3-对象深拷贝.js
@@ -12,14 +12,31 @@ var data = {
     },
     play: function() {    console.log("玩滑板");  }
 }
-// 一：object.create() （不能实现
+/**
+* @param target 粘贴到
+* @param source 复制
+* */
+function deepClone(target, source) {
+    // 1. target即每个object类型存放地址
+    if (!target) {
+        target = {};
+    }
 
-// 二：扩展运算符
-let data3 = {...data};
+    // 2. 遍历source
+    for (const key in source) {
+        // 2.1 source[key]为普通类型——直接存入target对象
+        if (typeof source[key] !== "object") {
+            target[key] = source[key];
+        }
+        // 2.2 source[key]为引用类型——递归
+        else {
+            target[key] = deepClone(target[key], source[key]);
+        }
+    }
+    return target;
+}
 
-/* 浅拷贝——指向同一地址：*/
-// let data4 = data;
-// data4.age = 20;  // data.age -> 20
-/* 深拷贝——不同地址： */
-data3.age = 19;
-console.log(data.age, data.name, data3.age, data3.name)
+let data1 = deepClone(undefined, data);
+data1.education = ["test"]  // 但是数组全复制成了对象
+console.log("data:", data)
+console.log("data1:", data1)
